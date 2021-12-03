877691ea7d3903cf3bc6ffa12dcc4a286d72650a
Jakub Rzeszutko <kubarzeszutko@gmail.com>
2021-12-03 15:57:24 +0100

    Add home page styling

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
     res.locals.currentUser = req.user
+    res.locals.currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl
     next()
 })
 
